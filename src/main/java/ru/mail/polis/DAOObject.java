4f56c634dbeca46c83b71748f7056b03c033f225
Yaroslav Erokhin <yaroslav98er@gmail.com>
2019-04-17 00:46:24 +0300

    modifier final was added

--- src/main/java/ru/mail/polis/DAOObject.java
+++ src/main/java/ru/mail/polis/DAOObject.java
@@ -13,17 +13,17 @@ public class DAOObject implements DAO {
 
   @NotNull
   @Override
-  public Iterator<Record> iterator(@NotNull ByteBuffer from) throws IOException {
+  public Iterator<Record> iterator(@NotNull final ByteBuffer from) throws IOException {
     return map.tailMap(from).values().iterator();
   }
 
   @Override
-  public void upsert(@NotNull ByteBuffer key, @NotNull ByteBuffer value) throws IOException {
+  public void upsert(@NotNull final ByteBuffer key, @NotNull final ByteBuffer value) throws IOException {
     map.put(key, Record.of(key, value));
   }
 
   @Override
-  public void remove(@NotNull ByteBuffer key) throws IOException {
+  public void remove(@NotNull final ByteBuffer key) throws IOException {
     map.remove(key);
   }
 
